92927bbcf362143a2a29687666296fa87db6daa6
Duck761 <dylanmesho1@gmail.com>
2022-08-01 16:31:39 -0400

    Coded ship factory and tests

---
+++ src/factories/Ship.js
@@ -0,0 +1,31 @@
+const shipFactory = (length) => {
+    let hitBox = [];
+    let sunk = false;
+  
+    //Initialize hitbox based on length
+    for (let i = 0; i < length; i++) {
+      hitBox.push(false);
+    }
+  
+    //Check if all postions are hit
+    const isSunk = () => {
+      for (let i = 0; i < length; i++) {
+        if (hitBox[i] === false) return false;
+      }
+  
+      return true;
+    };
+  
+    const hit = (position) => {
+      hitBox[position] = true;
+    };
+  
+    //Getters
+    const getLength = () => {
+      return length;
+    };
+  
+    return { length, isSunk, hit, hitBox };
+  };
+
+module.exports = shipFactory;
